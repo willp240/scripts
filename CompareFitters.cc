430de53b2d8e409ec99e6c199bdc9be957da9ac6
William Parker ( Research - Research Assistant ) <parkerw@pplxint10.physics.ox.ac.uk>
2020-09-10 20:31:58 +0100

    update compare pdf and fitter performance plotting scripts

--- CompareFitters.cc
+++ CompareFitters.cc
@@ -49,3 +49,103 @@ void Compare3Fitters(){
   t1->Draw("same");
 
 }
+
+void Compare4Fitters(){
+
+  //  TCanvas c1("c1", "c1", 1024, 768);
+
+  std::string title = "Electrons Simulated in Whole Scintillator";
+  //  std::string title = "Electrons with R > 1.5m";
+  //std::string title = "Electrons with R < 1.5m";
+
+  //  std::string name = "Rgt1.5_RecoordBiases1000.png";
+  //  std::string name = "Rlt1.5_RecoordBiases1000.png";
+  std::string name = "AllR_RecoordBiases1000.png";
+
+  TFile *_fileScint = TFile::Open("scintFitter.root");
+  TFile *_fileAll = TFile::Open("partialTestFitter_1Z.root");
+  TFile *_file2Z = TFile::Open("partialTestFitter_2Z.root");
+  TFile *_file4Z = TFile::Open("partialTestFitter_4Z.root");
+
+  TGraphErrors* hScint = (TGraphErrors*)_fileScint->Get("z_bias")->Clone();
+  TGraphErrors* hAll = (TGraphErrors*)_fileAll->Get("z_bias")->Clone();
+  TGraphErrors* h2Z = (TGraphErrors*)_file2Z->Get("z_bias")->Clone();
+  TGraphErrors* h4Z = (TGraphErrors*)_file4Z->Get("z_bias")->Clone();
+
+  hScint->SetLineColor(kRed);
+  hAll->SetLineColor(kBlack);
+  h2Z->SetLineColor(kBlue);
+  h4Z->SetLineColor(kGreen);
+  
+  TLegend* t1 = new TLegend( 0.6, 0.7, 0.9, 0.9 );
+  t1->AddEntry( hScint, "ScintFitter", "l" );
+  t1->AddEntry( hAll, "1 PDF", "l" );
+  t1->AddEntry( h2Z, "2 PDFs", "l" );
+  t1->AddEntry( h4Z, "4 PDFs", "l" );
+
+  hScint->GetYaxis()->SetRangeUser(-600,600);
+  //hAbove->GetXaxis()->SetTitle("Hit time residuals [ns]");
+  //hAbove->GetYaxis()->SetTitle("Normalised Counts");
+  hScint->SetTitle(title.c_str());
+
+  hScint->Draw("AP");
+  hAll->Draw("same P");
+  h2Z->Draw("same P");
+  h4Z->Draw("same P");
+  t1->Draw("same");
+
+  TLine *line = new TLine(hScint->GetXaxis()->GetXmax(),0,hScint->GetXaxis()->GetXmin(),0);
+  line->Draw("same");
+
+  //  c1.SaveAs(name.c_str());
+}
+
+void Compare4Resolutions(){
+
+  //  TCanvas c1("c1", "c1", 1024, 768);
+
+  std::string title = "Electrons Simulated in Whole Scintillator";
+  //std::string title = "Electrons with R > 1.5m";
+  //std::string title = "Electrons with R < 1.5m";
+
+  //std::string name = "Rgt1.5_RecoordRes1000.png";
+  //  std::string name = "Rlt1.5_RecoordRes1000.png";
+  std::string name = "AllR_RecoordRes1000.png";
+
+  TFile *_fileScint = TFile::Open("scintFitter.root");
+  TFile *_fileAll = TFile::Open("partialTestFitter_1Z.root");
+  TFile *_file2Z = TFile::Open("partialTestFitter_2Z.root");
+  TFile *_file4Z = TFile::Open("partialTestFitter_4Z.root");
+
+  TGraphErrors* hScint = (TGraphErrors*)_fileScint->Get("z_resolution")->Clone();
+  TGraphErrors* hAll = (TGraphErrors*)_fileAll->Get("z_resolution")->Clone();
+  TGraphErrors* h2Z = (TGraphErrors*)_file2Z->Get("z_resolution")->Clone();
+  TGraphErrors* h4Z = (TGraphErrors*)_file4Z->Get("z_resolution")->Clone();
+
+  hScint->SetLineColor(kRed);
+  hAll->SetLineColor(kBlack);
+  h2Z->SetLineColor(kBlue);
+  h4Z->SetLineColor(kGreen);
+
+  TLegend* t1 = new TLegend( 0.6, 0.7, 0.9, 0.9 );
+  t1->AddEntry( hScint, "ScintFitter", "l" );
+  t1->AddEntry( hAll, "1 PDF", "l" );
+  t1->AddEntry( h2Z, "2 PDFs", "l" );
+  t1->AddEntry( h4Z, "4 PDFs", "l" );
+
+  hScint->GetYaxis()->SetRangeUser(0,150);
+  //hAbove->GetXaxis()->SetTitle("Hit time residuals [ns]");
+  //hAbove->GetYaxis()->SetTitle("Normalised Counts");
+  hScint->SetTitle(title.c_str());
+
+  hScint->Draw("AP");
+  hAll->Draw("same P");
+  h2Z->Draw("same P");
+  h4Z->Draw("same P");
+  t1->Draw("same AP");
+
+  TLine *line = new TLine(hScint->GetXaxis()->GetXmax(),0,hScint->GetXaxis()->GetXmin(),0);
+  line->Draw("same");
+
+   //c1.SaveAs(name.c_str());
+}
