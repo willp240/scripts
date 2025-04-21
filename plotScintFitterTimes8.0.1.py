8d5abc727a12748fc7b4c39f31931c04797d4654
parkerw <william.parker@physics.ox.ac.uk>
2025-04-21 12:33:35 +0100

    linecount in plot scintfitter times

--- plotScintFitterTimes8.0.1.py
+++ plotScintFitterTimes8.0.1.py
@@ -3,19 +3,23 @@ import matplotlib.pyplot as plt
 plt.rcParams['font.size'] = 15.0
 
 # Define the log file path
-log_file = "evs8.0.1_labppo_2p2_bismsb_2p2_scintillator.txt"
+log_file = "/data/snoplus/parkerw/ratSimulations/Mar31_SFtime/output/merged.output"
 
 # Define the categories we are interested in
-categories = ["PMT Selector", "Muon Check", "Unseeded Classifiers", "Quad Seed", "MultiPDF", "Seeded Classifiers", "Energy", "Energy Seeded Classifiers"]
-#categories = ["Isotropy","ITR","XSite","QPDT","Mean Time","Iso Regions","Early Time","BiPo Cumul","BiPo LLH","AlphaBeta 212","AlphaBeta 214","BAB","Ext0Nu","Alpha N IBD"]
-#colors = ["Red", "Orange", "Purple", "Magenta", "Pink", "Yellow", "Beige", "White", "LightGreen", "Green", "Cyan", "Blue", "Grey", "Black" ]
-colors = ["Red", "Purple", "Pink", "Yellow", "Cyan", "Blue", "Green", "Black"]
+#categories = ["PMT Selector", "Muon Check", "Unseeded Classifiers", "Quad Seed", "MultiPDF", "Seeded Classifiers", "Energy", "Energy Seeded Classifiers"]
+categories = ["Isotropy","ITR","XSite","QPDT","Mean Time","Iso Regions","Early Time","BiPo Cumul","BiPo LLH","AlphaBeta 212","AlphaBeta 214","BAB","Ext0Nu","Alpha N IBD"]
+colors = ["Red", "Orange", "Purple", "Magenta", "Pink", "Yellow", "Beige", "White", "LightGreen", "Green", "Cyan", "Blue", "Grey", "Black" ]
+#colors = ["Red", "Purple", "Pink", "Yellow", "Cyan", "Blue", "Green", "Black"]
 
 values = {cat: [] for cat in categories}  # Store values for averaging
 
 # Read the file and extract relevant values
+linecount = 0
 with open(log_file, "r") as f:
     for line in f:
+        if linecount % 1000000 == 0:
+            print(linecount)
+        linecount = linecount + 1
         match = re.match(r"([\w\s]+):\s+([-+]?[\d\.e-]+)", line)
         if match:
             key, value = match.groups()
@@ -53,6 +57,6 @@ ax.legend(wedges, full_labels, bbox_to_anchor=(1,0.5), loc="center right", fonts
 plt.subplots_adjust(left=0.0, bottom=0.1, right=0.45)
 
 plt.title("ScintFitter Times (s)")
-#plt.savefig("RAT8.0.1_SeedClassBisMSB.pdf", format="pdf")
-plt.savefig("RAT8.0.1_ScintFitterBisMSB.pdf", format="pdf")
+plt.savefig("RAT8.0.1_SeedClassBisMSB.pdf", format="pdf")
+#plt.savefig("RAT8.0.1_ScintFitterBisMSB.pdf", format="pdf")
 plt.show()
