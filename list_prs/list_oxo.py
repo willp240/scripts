5bba44c5c6e8879dff81cd94b8bad274ac939fb8
parkerw <william.parker@physics.ox.ac.uk>
2025-12-07 02:15:09 +0000

    list oxo python 3

--- list_prs/list_oxo.py
+++ list_prs/list_oxo.py
@@ -12,10 +12,10 @@ buffer = StringIO()
 headers = { 'Authorization' : 'Basic %s' % base64.b64encode("willp240:"), 'Accept' : 'application/vnd.github.mercy-preview+json'}
 base_url="https://api.github.com/search/issues"
 repo = "snoplus/oxsx"
-date = "2021-03-04"
+date = "2024-08-27"
 query = "sort=updated&per_page=100&q=repo:{repo}+type:pr+is:merged+merged:>={date}".format(repo=repo,date=date)
 parsed_url="{base}?{query}".format(base=base_url,query=query)
-print "Testing URL : ", parsed_url
+print ("Testing URL : ", parsed_url)
 #sys.exit(0)
 c = pycurl.Curl()
 c.setopt(c.HTTPHEADER, ["%s: %s" % t for t in headers.items()])
@@ -27,18 +27,18 @@ c.perform()
 c.close()
 body = buffer.getvalue()
 
-print body
+print (body)
 # Body is a string in some encoding.
 # In Python 2, we can print it without knowing what the encoding is.
-print ""
-print ""
-print ""
-print ""
-print ""
-print ""
-print "==== Here's the answer ==="
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("")
+print ("==== Here's the answer ===")
 response = json.loads(body)
-print "Found {0} PRs that have been merged".format(response["total_count"])
+print ("Found {0} PRs that have been merged".format(response["total_count"]))
 # Pretty print:
 for pr in response["items"]:
-  print '* #{num} : {title}'.format(num=pr['number'], title=pr['title'].encode('utf-8'))
+  print ('* #{num} : {title}'.format(num=pr['number'], title=pr['title'].encode('utf-8')))
